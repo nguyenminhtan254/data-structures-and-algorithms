98246378c0d784d5457df8e1b3b4263f8ffd4b34
Minh Tan Nguyen <nguyenminhtan250406@gmail.com>
2026-07-02 15:07:47 -0700

    add Tree, BinaryTree

---
+++ DataStructures/java/Tree/BinaryTree/LinkedBinaryTree.java
@@ -0,0 +1,72 @@
+package DataStructures.java.Tree.BinaryTree;
+
+import java.util.Iterator;
+
+/**
+ * LinkedBinaryTree implements the BinaryTreeADT interface
+ *
+ * @author Minh Tan Nguyen
+ */
+public class LinkedBinaryTree<T> implements BinaryTreeADT<T> {
+
+    @Override
+    public T getRootElement() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRootElement'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean contains(T targetElement) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public T find(T targetElement) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'find'");
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public Iterator<T> iteratorInOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iteratorInOrder'");
+    }
+
+    @Override
+    public Iterator<T> iteratorPreOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iteratorPreOrder'");
+    }
+
+    @Override
+    public Iterator<T> iteratorPostOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iteratorPostOrder'");
+    }
+
+    @Override
+    public Iterator<T> iteratorLevelOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iteratorLevelOrder'");
+    }
+
+}
---
+++ DataStructures/java/Tree/Tree.md
@@ -0,0 +1,29 @@
+# Tree
+
+## Definition
+- A tree is a non-linear structure in which elements are organized into a hierarchy.
+- A tree is comprised of a set of nodes in which elements are stored and edges connect one node to another.
+- *General trees* have no limit to the number of children a node may have.
+- A tree that limits each node to no more than n children is referred to as an n-ary tree.
+
+## Terminologies
+- **Parent node**: A node that is an immediate predecessor of another node.
+- **Child node**: A node that is an immediate successor of another node.
+- **Root node**: The topmost node in the tree, which does not have a parent.
+- **Leaf node**: Nodes that do not have any children.
+- **Internal node**: A node that is not the root and has at least one child.
+- **Subtree**: A tree structure makes up part of another tree.
+- **Level of a Node**: The number of edges in the path from the root to that node. The root node is at level 0.
+- **Height of a Tree**: The length of the longest path from the root to the a leaf.
+
+# Binary Tree
+
+## Definition
+- A binary tree is a tree in which each node may have at most 2 children.
+
+## Classifying Trees
+
+## Tree Traversals
+
+# Binary Search Tree
+
